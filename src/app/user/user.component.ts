c87fc09d620032d804455b3d6ebe02ac77c1e854
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-06-08 15:34:45 +0700

    istimiwir

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,11 +9,18 @@ import { ThrowStmt } from '@angular/compiler';
 })
 export class UserComponent implements OnInit {
 
+  // displaying data in html
+  Users :any;
+
+  // materi consume rest api
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    // materi consume rest api
    let resp= this.http.get("https://jsonplaceholder.typicode.com/Users");
-   resp.subscribe((data)=>console.log(data));
+  //  resp.subscribe((data)=>console.log(data));
+  // displayng data in html
+  resp.subscribe((data)=>this.Users=data);
   }
 
 }
